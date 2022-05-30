cd6e49348bcd5fac5e27d13cce06a73258a947bb
Cristofer Jaimez <jaimez07788@gmail.com>
2022-05-30 17:16:01 -0500

    mapa de calor

--- public/js/control.js
+++ public/js/control.js
@@ -19,9 +19,13 @@ $( document ).on( 'click', '.fv', function(e){
 //mapa de calor
 function calor() {
     var heat = L.heatLayer([
-        [4.570868, -74.297333, 112171931937917491474474719471], // lat, lng, intensity
+        [4.570868, -74.297333, 1474474719471],
+        [4.470868, -73.297333, 14744741] // lat, lng, intensity
         
     ],
-    {radius: 25}).addTo(map);
+    {
+        radius: 25,
+        blur : 15, // default value
+    }).addTo(map);
     
 }
