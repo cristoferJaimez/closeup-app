2a870fb6d8aee7ee93a99bdeb5afba3ccc37b628
Cristofer Jaimez <jaimez07788@gmail.com>
2022-06-14 22:03:06 -0500

    front

--- resources/views/home/home_.blade.php
+++ resources/views/home/home_.blade.php
@@ -2,12 +2,13 @@
 @section('contenido')
     @include('home.navbar')
     @include('home.navbutton')
-
+    @include('home.load')
     <!--msm -->
     <style>
         .socialIcon {
             display: none;
         }
+
     </style>
     <div class="container" style="">
         <div class="row mt-5 ">
@@ -48,117 +49,126 @@
 
 
                 @foreach ($posts as $post)
-                @if ($post->user_id === auth()->user()->id)
-                <div class=" card mb-3 ">
-                    <div class="card-header no-border">
-                        <h5 class="card-title">{{$post->title}}</h5>
-                    </div>
-                    <div class="card-body pt-0">
-                        <div class="widget-49">
-                            <div class="widget-49-title-wrapper">
+                    @if ($post->user_id === auth()->user()->id)
+                        <div class=" card mb-3 ">
+                            <div class="card-header no-border">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                            </div>
+                            <div class="card-body pt-0">
+                                <div class="widget-49">
+                                    <div class="widget-49-title-wrapper">
 
                                         @if ($post->title === 'Flash Farmacéutico')
-                                        <div class="widget-49-date-danger">
-                                            <span class="widget-49-date-day">
-                                        <i class="fa-solid fa-bolt-lightning"></i>
-
-                                        @else
-                                        <div class="widget-49-date-success">
-                                            <span class="widget-49-date-day">
-                                        <i class="fa-solid fa-prescription"></i>
-
+                                            <div class="widget-49-date-danger">
+                                                <span class="widget-49-date-day">
+                                                    <i class="fa-solid fa-bolt-lightning"></i>
+                                                @else
+                                                    <div class="widget-49-date-success">
+                                                        <span class="widget-49-date-day">
+                                                            <i class="fa-solid fa-prescription"></i>
                                         @endif
-                                    </span>
-                                    <span class="widget-49-date-month"></span>
-                                </div>
+                                        </span>
+                                        <span class="widget-49-date-month"></span>
+                                    </div>
 
 
-                                    @if ($post->user_id === auth()->user()->id )
+                                    @if ($post->user_id === auth()->user()->id)
                                         <div class="widget-49-meeting-info">
-                                            <span class="widget-49-pro-title">{{$post->title}}</span>
+                                            <span class="widget-49-pro-title">{{ $post->title }}</span>
                                             <span class="widget-49-meeting-time">{{ $post->created_at }}</span>
                                         </div>
 
-                            </div>
-                            <ol class="widget-49-meeting-points">
-                                <!--<li class="widget-49-meeting-item"><span>descripcion</span></li>-->
-                            </ol>
-                            <div class="widget-49-meeting-action">
-                                <a href="{{ $post->url }}" target="iframe" onclick="hide()"
-                                    class="btn btn-sm btn-flash-border-success">View All</a>
-                            </div>
+                                </div>
+                                <ol class="widget-49-meeting-points">
+                                    <!--<li class="widget-49-meeting-item"><span>descripcion</span></li>-->
+                                </ol>
+                                <div class="widget-49-meeting-action">
+                                    <a href="{{ $post->url }}" target="iframe" onclick="hide()"
+                                        class="btn btn-sm btn-flash-border-success">View All</a>
+                                </div>
+                    @endif
 
-                            @else
-                                <i class="fa-solid fa-ban fa-9x"></i>
-                            aqui
-                            @endif
+            </div>
+        </div>
+    </div>
+@else
+<div class="col-md-10 col-sm-12  mt-5" style="position: relative; font-size: 0.7em;">
 
-                            </div>
-                        </div>
-                    </div>
-                @endif
+    <div class=" row mt-5" id="default"  >
+        <div class="text-center text-muted position-absolute top-50 start-50 translate-middle"
+            style="background-image: url('')" >
+            <i class="fa-solid fa-face-grin-beam-sweat fa-9x cometa"></i>
+            <br>
+            <i class=""></i>
+            <b>not post...</b>
+        </div>
+
+    </div>
+</div>
+@break
+@endif
+@endforeach
+</div>
+
+</div>
+
+
+
+</div>
+</div>
+
+<div class="position-absolute top-50 end-0 translate-middle-y " style="margin-right: 4px;  z-index: 999999">
+    <button class="btn btn-outline-primary   rounded-circle "
+        style="margin: auto; text-align:center; 35px; height: 35px;" onclick="body.requestFullscreen()">
+        <i class="fa-solid fa-expand"></i>
+    </button>
+    <br>
+    <button class="btn btn-outline-danger btn_   rounded-circle "
+        style="display:none; text-align:center; 35px; height: 35px;" onclick="hide_iframe()">
+        <i class="fa-solid fa-circle-xmark"></i>
+    </button>
+
+</div>
+
+<div class="position-absolute bottom-0 start-50 translate-middle-x next_prev_  d-none p-2 "
+    style="display: none; z-index: 999999">
+    <div class="container">
+        <div class="row">
+
+            <div class="  col-sm-12 col-md-4 ">
+                <button class="btn btn-outline-primary   rounded-circle btn_prev "
+                    style="margin: auto; text-align:center; 35px; height: 35px;" onclick="">
+                    <i class="fa-solid fa-angle-left"></i>
+                </button>
+            </div>
+
+            <div class="  col-sm-12 col-md-4">
+                @foreach ($proveedor as $pro)
+                    @if ($pro->id === auth()->user()->id)
+                        <img class="img-fluid " style="" src="{{ $pro->url }}" />
+                    @endif
                 @endforeach
-                </div>
-
             </div>
 
+            <div class="  col-sm-12 col-md-4 text-end">
+                <button class="btn btn-outline-primary   rounded-circle  btn_next"
+                    style="margin: auto; text-align:center; 35px; height: 35px;" onclick="">
+                    <i class="fa-solid fa-angle-right"></i>
+                </button>
+            </div>
 
 
         </div>
     </div>
 
-    <div class="position-absolute top-50 end-0 translate-middle-y " style="margin-right: 4px;  z-index: 999999">
-        <button class="btn btn-outline-primary   rounded-circle "
-            style="margin: auto; text-align:center; 35px; height: 35px;" onclick="body.requestFullscreen()">
-            <i class="fa-solid fa-expand"></i>
-        </button>
-        <br>
-        <button class="btn btn-outline-danger btn_   rounded-circle "
-            style="display:none; text-align:center; 35px; height: 35px;" onclick="hide_iframe()">
-            <i class="fa-solid fa-circle-xmark"></i>
-        </button>
-
-    </div>
-
-    <div class="position-absolute bottom-0 start-50 translate-middle-x next_prev_  d-none p-2 "
-        style="display: none; z-index: 999999">
-        <div class="container">
-            <div class="row">
-
-                <div class="  col-sm-12 col-md-4 ">
-                    <button class="btn btn-outline-primary   rounded-circle btn_prev "
-                        style="margin: auto; text-align:center; 35px; height: 35px;" onclick="">
-                        <i class="fa-solid fa-angle-left"></i>
-                    </button>
-                </div>
-
-                <div class="  col-sm-12 col-md-4">
-                    @foreach ($proveedor as $pro)
-                        @if ($pro->id === auth()->user()->id)
-                            <img class="img-fluid " style="" src="{{ $pro->url }}"  />
-                        @endif
-                    @endforeach
-                </div>
-
-                <div class="  col-sm-12 col-md-4 text-end">
-                    <button class="btn btn-outline-primary   rounded-circle  btn_next"
-                        style="margin: auto; text-align:center; 35px; height: 35px;" onclick="">
-                        <i class="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
-
-
-            </div>
-        </div>
-
-    </div>
+</div>
 
 
 
 
-    <iframe src="" frameborder="0" class="iframe " id="iframe" onload=""
-        style="overflow: auto;position:fixed; top:60px; left:0px; bottom:0px; right:0px; width:100%; height:99%; border:none; margin:0; padding:0; overflow:hidden; z-index:999998; display: none;"
-        name="iframe">
+<iframe src="" frameborder="0" class="iframe " id="iframe" onload=""
+    style="overflow: auto;position:fixed; top:60px; left:0px; bottom:0px; right:0px; width:100%; height:99%; border:none; margin:0; padding:0; overflow:hidden; z-index:999998; display: none;"
+    name="iframe">
 
-    </iframe>
+</iframe>
 @endsection
