e50ee9121c626dadcf80c9b90e142568ff60709c
cristoferjaimez <jaimez07788@gmail.com>
2022-07-13 11:03:27 -0500

    login bug correction

--- resources/views/auth/loginOK.blade.php
+++ resources/views/auth/loginOK.blade.php
@@ -1,5 +1,5 @@
 
-        <form action="{{route('home')}}">
+        <form action="{{route('home')}}" method="POST">
             @csrf
             <button type="submit" class="btn badge bg-danger text-wrap">
                 <i class="fa-solid fa-backward"></i> {{auth()->user()->name}},   return home.</button>
