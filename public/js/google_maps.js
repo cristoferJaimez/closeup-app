2f373ff2b7ff02f918c191324ad42c004a0e2c1c
cristoferjaimez <jaimez07788@gmail.com>
2022-07-14 14:46:12 -0500

    mapa json fuera

--- public/js/google_maps.js
+++ public/js/google_maps.js
@@ -153,7 +153,7 @@ function darwing(near_place) {
         { location: 'Caldas', cod: '17', api: 'caldas.json' },
         { location: 'Caquetá', cod: '18', api: 'caqueta.json' },
         { location: 'Cauca', cod: '19', api: 'cauca.json' },
-        { location: 'Cesar', cod: '20', api: 'cesar.json' },
+        { location: 'Cesar', cod: '20', api: 'casar.json' },
         { location: 'Córdoba', cod: '23', api: 'cordoba.json' },
         { location: 'Chocó', cod: '27', api: 'choco.json' },
 
@@ -168,6 +168,7 @@ function darwing(near_place) {
         { location: 'Sucre', cod: '70', api: 'sucre.json' },
         { location: 'Tolima', cod: '73', api: 'tolima.json' },
         { location: 'Valle del Cauca', cod: '76', api: 'valle_cauca.json' },
+        { location: 'Alban', cod: '52', api: 'narino.json' },
     ]
     var resultado;
     var BreakException = {};
